7edcd890a5851ea1c12b756590d91fd2dd5cd9e5
Shlomi Koren <shlomi@korent.co.il>
2020-02-04 21:00:34 +0200

    Module 2 complete

--- BethanysPieShopHRM.Server/Startup.cs
+++ BethanysPieShopHRM.Server/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BethanysPieShopHRM.Server.Data;
 
 namespace BethanysPieShopHRM.Server
 {
@@ -27,8 +20,11 @@ namespace BethanysPieShopHRM.Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            //.AddCircuitOptions(options =>{options.DetailedErrors = true;}); - will enable exception details on the browser
+            services.AddServerSideBlazor().AddCircuitOptions(options =>
+            {
+                options.DetailedErrors = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
